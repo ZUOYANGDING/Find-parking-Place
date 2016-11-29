cdd3a9936b717bf7a9339b0e2b8b57c74f754c49
DingZuoyang <zuoyangding@pal-nat186-45-157.itap.purdue.edu>
2016-11-28 21:53:43 -0500

    test

--- Find-Parking-Place/app.js
+++ Find-Parking-Place/app.js
@@ -3,15 +3,20 @@
  * Generated Node.js application that can run on IBM Bluemix
  */
 
-var http = require("http");
+// var http = require("http");
 
-var appport = process.env.VCAP_APP_PORT || 8888;
+// var appport = process.env.VCAP_APP_PORT || 8888;
 
-http.createServer(function(request, response) {
+// http.createServer(function(request, response) {
 
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.write("Generated Node.js application that runs on IBM Bluemix\n");
-    response.write("hahaha test");
-    response.end();	
+//     response.writeHead(200, {"Content-Type": "text/plain"});
+//     response.write("Generated Node.js application that runs on IBM Bluemix\n");
+//     response.write("hahaha test");
+//     response.end();	
 
-}).listen(appport);
+// }).listen(appport);
+var express = require('express');
+var app = express();
+
+app.use(express.static(__dirname + '/public'));
+app.listen(3000);
